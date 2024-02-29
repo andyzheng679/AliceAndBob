0faec13970ce8982a0a878be9d4980c431ba92ad
andyzheng679 <andyzheng679@gmail.com>
2024-02-29 14:04:51 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,16 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
 
         // call the compute() method on that new object
+        Main mainobj = new Main();
+        mainobj.compute();
+
     }
 
     public void compute() {
@@ -18,6 +22,15 @@ public class Main {
         // Print greeting to screen if Alice or Bob are true
         
         // Print "You are neither Alice nor Bob." otherwise
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your name: ");
+        String getName = scanner.nextLine();
+
+        if(getName.equals("Alice") || getName.equals("Bob") || getName.equals("alice") || getName.equals("bob")){
+            System.out.println("Hello" + getName);
+        }else{
+            System.out.println("You are neither Alice or Bob");
+        }
     }
 }
